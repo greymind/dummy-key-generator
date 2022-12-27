aa92e11f46f331eae8f0f7b45b1934a9fd247f61
Balakrishnan (Balki) Ranganathan <balki@live.com>
2022-12-27 12:34:54 +0100

    changed to substring

--- index.mjs
+++ index.mjs
@@ -1,8 +1,7 @@
 import moment from "moment";
-import { program } from "commander"
+import { program } from "commander";
 
-program
-	.option('-k, --keys <keys>', 'Number of keys to generate', 10)
+program.option("-k, --keys <keys>", "Number of keys to generate", 10);
 
 program.parse();
 
@@ -13,12 +12,14 @@ let filename = `keys-${now.toString()}.txt`;
 console.log(`Generating ${options.keys} keys to ${filename}...`);
 
 let fragment = (length) => {
-  return Math.random().toString(36).substr(2, length);
+  return Math.random()
+    .toString(36)
+    .substring(2, 2 + length);
 };
 
 for (let index = 0; index < options.keys; index++) {
-	let key = `${fragment(5)}-${fragment(5)}-${fragment(5)}`.toUpperCase();	
-	console.log(key);
+  let key = `${fragment(5)}-${fragment(5)}-${fragment(5)}`.toUpperCase();
+  console.log(key);
 }
 
 console.log(`Generation complete!`);
